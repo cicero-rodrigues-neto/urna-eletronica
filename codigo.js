18496981eb2eba414f23ebdb44c1c0733dc2d976
Cicero Rodrigues <99734384+cicero-rodrigues-neto@users.noreply.github.com>
2022-03-21 14:31:19 -0300

    Add files via upload

---
+++ codigo.js
@@ -0,0 +1,42 @@
+function inserir(valor) {
+  var valor1 = document.getElementById("campo1").value;
+  var valor2 = document.getElementById("campo2").value;
+
+  if (valor1 == "") {
+    document.getElementById("campo1").value = valor;
+  } else if (valor2 == "") {
+    document.getElementById("campo2").value = valor;
+  }
+}
+
+function corrige() {
+  document.getElementById("campo1").value = "";
+  document.getElementById("campo2").value = "";
+}
+
+
+function votar() {
+
+  var valor1 = parseInt(document.getElementById("campo1").value);
+  var valor2 = parseInt(document.getElementById("campo2").value);
+  var candidado = (valor1 * 10) + valor2;
+  if (sessionStorage.getItem(candidado) !== null) {
+    votos = parseInt(sessionStorage.getItem(candidado)) + 1;
+    sessionStorage.setItem(candidado, votos);
+  } else {
+    sessionStorage.setItem(candidado, 1);
+  }
+  alert("Voto Computado")
+  document.getElementById("campo1").value = "";
+  document.getElementById("campo2").value = "";
+}
+
+function resultado() {
+  document.getElementById("campo-2").innerHTML = ""
+  for (i = 0; i < 100; i++) {
+    if (sessionStorage.getItem(i) !== null) {
+      //alert(i);
+      document.getElementById("resultado").innerHTML += "Cantidado " + i + " tem " + sessionStorage.getItem(i) + " votos<br/>";
+    }
+  }
+}
